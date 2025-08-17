79e14661e9820159da654075f8cc4255b8b02635
Labham Sharma <labhamsharma1633@gmail.com>
2025-08-18 00:00:04 +0530

    frontend setup tanstack query setup and quick tutorial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Routes, Route } from "react-router-dom"; 
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import ChatPage from "./pages/ChatPage";
+import CallPage from "./pages/CallPage";
+import Onboarding from "./pages/Onboarding";
+import NotificationPage from "./pages/NotificationPage";
+import { Toaster } from "react-hot-toast";
+import { useEffect,useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { axiosInstance } from "./lib/axios";
+import { axiosInstance } from "./lib/axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  //axios
+  //react query tanstack query
+  const {data,isLoading,error}=useQuery({queryKey:["todos"],
+    queryFn:async()=>{
+      const res=await axiosInstance.get("/auth/me")
+      
+      return res.data;
+    },
+    retry:false,
+
+  });
+  console.log(data);
+  
+  
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="h-screen" data-theme="night">
+      
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/chat" element={<ChatPage/>}/>
+        <Route path="/call" element={<CallPage/>}/>
+        <Route path="/onboarding" element={<Onboarding/>}/>
+        <Route path="/notification" element={<NotificationPage/>}/>
 
-export default App
+
+
+      </Routes>
+      <Toaster/>
+      
+    </div>
+  );
+};
+
+export default App;
