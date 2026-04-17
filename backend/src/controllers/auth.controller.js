1d281ec71e38429a293db7015de190a3373ef68b
Labham Sharma <labham1633@gmail.com>
2026-04-17 13:25:19 +0530

    final auth fixes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -63,8 +63,8 @@ export async function signup(req,res){
         res.cookie("jwt",token,{
             maxAge:7*24*60*60*1000,
             httpOnly:true,
-            sameSite:"strict",
-            secure:process.env.NODE_ENV==="production"
+            sameSite:"None",
+            secure:true
         })
 
         res.status(201).json({success:true,user:newUser})
@@ -114,8 +114,8 @@ export async function login(req,res){
         res.cookie("jwt",token,{
             maxAge:7*24*60*60*1000,
             httpOnly:true,
-            sameSite:"strict",
-            secure:process.env.NODE_ENV==="production"
+            sameSite:"None",
+            secure:true
         })
 
         res.status(200).json({success:true,user});
@@ -132,7 +132,11 @@ export async function login(req,res){
     }
 }
 export async function logout(req,res){
-    res.clearCookie("jwt");
+    res.clearCookie("jwt",{
+        httpOnly:true,
+        sameSite:"None",
+        secure:true,
+    });
     res.status(200).json({success:true,message:"Logout Successful"})
 }
 
